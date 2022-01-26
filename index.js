0eea5659208bb02b9763ac1925d5525109d0060b
barrulik <barrulik@gmail.com>
2022-01-26 20:12:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const Fs = require('fs');
 const env = require('dotenv');
 
 const config = require('./config.json')
-const token = env.config().parsed.TOKEN;
+const token = env.config().parsed.token;
 
 
-const client = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES , Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS]});
+const client = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS]});
 
+client.once('ready', () => {
+    console.log(client.user.tag + ' is online');
+})
+
+client.on('interactionCreate', async interaction => {
+    console.log(interaction);
+})
+
+client.login(token);
