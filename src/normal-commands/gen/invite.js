317f3eaa1bc712de1508f5b937fceb3507546ffd
barrulik <barrulik@gmail.com>
2022-01-27 18:26:25 +0200

    added invite command

---
+++ src/normal-commands/gen/invite.js
@@ -0,0 +1,10 @@
+module.exports = {
+  config: {
+    name: "invite",
+    aliases: ["addbot"],
+    description: "",
+  },
+  async run(client, message) {
+    message.channel.send(`My invite link is ` + `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`);
+  },
+};
