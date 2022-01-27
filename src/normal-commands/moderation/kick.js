4c1d48934f3059d63f68e986e737c9bdff6e662c
barrulik <barrulik@gmail.com>
2022-01-27 21:47:06 +0200

    fixed kick

--- src/normal-commands/moderation/kick.js
+++ src/normal-commands/moderation/kick.js
@@ -2,29 +2,34 @@ module.exports = {
   config: {
     name: "kick",
     aliases: [],
-    description: "",
+    description: "Kicks people",
   },
   async run(client, message) {
     if (!message.guild.me.permissions.has(`KICK_MEMBERS`))
       return message.channel.send(`sorry, i dont have permission to kick members, please enable it in my role's settings`);
-    let targetMember = message.mentions.users.first();
+    let targetUser = message.mentions.users.first();
     if (!targetMember)
-      return message.channel.send(`sorry, couldnt find the guy that you were looking for`)
+      return message.channel.send(`sorry, couldnt find the guy that you were looking for`);
     if (client.user.id == targetMember.id)
-      return message.channel.send(`sorry, i cannot kick myself`)
+      return message.channel.send(`sorry, i cannot kick myself`);
     
     
-    targetMember = message.guild.members.cache.get(targetMember.id);
+    let targetMember = message.guild.members.cache.get(targetUser.id);
     let clientMember = message.guild.members.cache.get(client.user.id);
     //hierarchy
     if (clientMember.roles.highest.position < targetMember.roles.highest.position)
-      return message.channel.send(`sorry, i cannot kick this member since my role is too low in the hirrarchy`)
+      return message.channel.send(`sorry, i cannot kick this member since my role is too low in the hirrarchy`);
   
     let authorMember = message.guild.members.cache.get(message.author.id);
-    if (!targetMember.roles.highest.position || authorMember.roles.highest.position < targetMember.roles.highest.position)
-      return message.channel.send(`sorry, i cannot kick this member since your role is too low in the hirrarchy`)
-    
-    
-    targetMember.kick();
+    if (!targetMember.roles.highest.position || authorMember.roles.highest.position <= targetMember.roles.highest.position)
+      return message.channel.send(`sorry, i cannot kick this member since your role is too low/equal in the hirrarchy`);
+    let reason = message.args.slice(1).join(" ").trim();
+    if (reason)
+      reason=" for " + reason;
+      try {
+        targetMember.send(`you just got kicked` + reason + ` from ` + message.guild.name)
+      } catch {};
+      targetMember.kick();
+      message.channel.send(`successfully kicked ` + targetUser.tag + reason);
   },
 };
