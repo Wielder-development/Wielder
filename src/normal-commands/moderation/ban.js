08574944c8af0ffcc3a0fafec50fea83849fc86a
barrulik <barrulik@gmail.com>
2022-01-29 10:45:09 +0200

    integrated langs

--- src/normal-commands/moderation/ban.js
+++ src/normal-commands/moderation/ban.js
@@ -4,25 +4,26 @@ module.exports = {
     aliases: [],
     description: "Bans idiots",
   },
-  async run(client, message) {
+  async run(client, message, language) {
+    language = language.moderation.ban;
     if (!message.guild.me.permissions.has(`BAN_MEMBERS`))
-      return message.channel.send(`sorry, i dont have permission to ban members, please enable it in my role's settings`);
+      return message.channel.send(language.botMissingPerms);
     let targetUser = message.mentions.users.first();
     if (!targetUser)
-      return message.channel.send(`sorry, couldnt find the guy that you were looking for`);
+      return message.channel.send(language.doesntFind);
     if (client.user.id == targetUser.id)
-      return message.channel.send(`sorry, i cannot ban myself`);
+      return message.channel.send(language.selfAction);
     
     
     let targetMember = message.guild.members.cache.get(targetUser.id);
     let clientMember = message.guild.members.cache.get(client.user.id);
     //hierarchy
-    if (clientMember.roles.highest.position < targetMember.roles.highest.position)
-      return message.channel.send(`sorry, i cannot ban this member since my role is too low in the hirrarchy`);
+    if (clientMember.roles.highest.position <= targetMember.roles.highest.position)
+      return message.channel.send(language.botRoleTooLow);
   
     let authorMember = message.guild.members.cache.get(message.author.id);
     if (!targetMember.roles.highest.position || authorMember.roles.highest.position <= targetMember.roles.highest.position)
-      return message.channel.send(`sorry, i cannot ban this member since your role is too low/equal in the hirrarchy`);
+      return message.channel.send(language.authorRoleTooLowOrEqual);
     let reason = message.args.slice(1).join(" ").trim();
     if (reason)
       reason=" for " + reason;
