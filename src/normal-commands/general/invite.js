4bc7a44aec65e73e28636cbf6236d80ac982292a
barrulik <barrulik@gmail.com>
2022-01-29 10:44:55 +0200

    Update invite.js

--- src/normal-commands/general/invite.js
+++ src/normal-commands/general/invite.js
@@ -4,7 +4,7 @@ module.exports = {
     aliases: ["addbot"],
     description: "sends invite link for bot",
   },
-  async run(client, message) {
+  async run(client, message, language) {
     message.channel.send(`My invite link is ` + `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`);
   },
 };
