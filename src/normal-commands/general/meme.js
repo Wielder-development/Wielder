5c015ad51ad7741f53f605c55ecdcb92d7b89bd2
barrulik <barrulik@gmail.com>
2022-02-18 19:47:20 +0200

    faster meme command

--- src/normal-commands/general/meme.js
+++ src/normal-commands/general/meme.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js");
-const snekfetch = require("snekfetch");
-
-const { getPost, getImage } = require("random-reddit");
+const axios = require("axios");
 
 module.exports = {
   config: {
@@ -13,26 +11,23 @@ module.exports = {
   async run(client, message, language) {
     if (message.args.length == 0)
       return message.channel.send("invalid subreddit");
-    try {
-      const { body } = await snekfetch
-        .get(`https://www.reddit.com/r/${message.args[0]}.json?sort=top&t=day`)
-        .query({ limit: 100 });
-      const memes = body.data.children.filter(
-        (post) =>
-          !post.data.over_18 || !post.data.url.startsWith("https://v.redd.it/")
-      );
-      let meme = memes[Math.floor(Math.random() * memes.length)].data;
-      let embed = client.embeds.empty().setImage(meme.url);
-      embed.setDescription("" + meme.selftext);
-      meme = await getPost(message.args[0]);
-      //embed.setDescription("" + meme.selftext);
-      embed.title = meme.title;
+    
+      const res = await axios({ url: `https://www.reddit.com/r/${message.args[0]}.json?sort=top&t=day&limit=100` });
+      const memes = res.data.data.children.filter((child) => !child.data.url.startsWith("https://v.redd.it/"));
+      if (!memes.length) return message.channel.send("there arent any memes on this subreddit");
+      const randomMeme = memes[Math.floor(Math.random() * memes.length)].data;
+      if (!message.channel.nsfw)
+        while (randomMeme.over_18){
+          memes.splice(memes.indexOf(randomMeme),1);
+          if (memes.length==0)
+            return message.channel.send("Sorry, I have ran out of memes because of the nsfw setting")
+          randomMeme = memes[Math.floor(Math.random() * memes.length)].data;
+        }
+  
+      
+      let embed = client.embeds.empty().setImage(randomMeme.url);
+      embed.setDescription("" + randomMeme.selftext);
+      embed.title = randomMeme.title;
       message.channel.send({ embeds: [embed] });
-    } catch (err) {
-      console.log(err);
-      return message.channel.send(
-        "Sorry, i couldnt find the subreddit that you were refering to"
-      );
-    }
   },
 };
