ffeea93fa765c4cd899bf1b7c4222dcac2e23e4e
barrulik <barrulik@gmail.com>
2022-02-23 21:34:22 +0200

    nsfw subreddit block

--- src/normal-commands/general/meme.js
+++ src/normal-commands/general/meme.js
@@ -11,8 +11,11 @@ module.exports = {
   async run(client, message, language) {
     if (message.args.length == 0)
       return message.channel.send("invalid subreddit");
-    
-    const res = await axios({ url: `https://www.reddit.com/r/${message.args[0]}.json?sort=top&t=day&limit=50${message.channel.nsfw?'&include_over_18=on' : '&include_over_18=off'}`});
+    const subreddit = message.args[0].toLowerCase();
+    if (subreddit == "nsfw" && !message.channel.nsfw)
+      return message.channel.send("wtf");
+
+    const res = await axios({ url: `https://www.reddit.com/r/${subreddit}.json?sort=top&t=day&limit=50${message.channel.nsfw?'&include_over_18=on' : '&include_over_18=off'}`});
     const memes = res.data.data.children.filter((child) => !child.data.url.startsWith("https://v.redd.it/"));
     if (!memes.length) return message.channel.send("there arent any memes on this subreddit");
     const randomMeme = memes[Math.floor(Math.random() * memes.length)].data;
